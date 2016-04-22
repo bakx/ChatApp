4801cc9b3028b442acd3a01984b0f0790a338b5c
Gideon Bakx <code@bakx.ca>
2016-04-21 23:05:33 -0400

    Initial commit. Simple chat hub using SignalR2 + MVC.

---
+++ ChatApp/ChatServer/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatServer
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.SendMessage(name, message);
+        }
+    }
+}
